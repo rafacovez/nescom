03173db8f8fabdac76e34248e38bc90511b78d88
Adán Estévez <adanescollante@gmail.com>
2023-05-25 16:42:55 -0400

    created layouts folder and coded primary layout

---
+++ src/layouts/PrimaryLayout.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="wrapper">
+    <HeaderSection />
+    <slot></slot>
+    <FooterSection />
+  </div>
+</template>
+
+<script>
+import HeaderSection from "../components/HeaderSection.vue";
+import FooterSection from "../components/FooterSection.vue";
+
+export default {
+  name: "PrimaryLayout",
+  components: {
+    HeaderSection,
+    FooterSection,
+  },
+};
+</script>
