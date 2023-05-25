de2865ae4d6aa525881252fad98651b440aeee8c
Adán Estévez <adanescollante@gmail.com>
2023-05-24 23:18:23 -0400

    coded footer section and added colors and fonts

---
+++ src/components/FooterSection.vue
@@ -0,0 +1,42 @@
+<template>
+  <footer class="footer">
+    <ul>
+      <li>item</li>
+      <li>item</li>
+      <li>item</li>
+    </ul>
+    <div class="sub-footer">
+      <p>Copyright blah blah</p>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "FooterSection",
+};
+</script>
+
+<style>
+.footer {
+  background-color: var(--dark-gray);
+  color: var(--white);
+}
+
+.footer > ul {
+  font-size: calc(var(--font-size) * 0.9);
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+  padding: 2rem;
+  margin: 0;
+}
+
+.sub-footer {
+  font-size: calc(var(--font-size) * 0.8);
+  background-color: var(--black);
+  color: var(--gray);
+  padding: 2rem;
+  text-align: center;
+}
+</style>
