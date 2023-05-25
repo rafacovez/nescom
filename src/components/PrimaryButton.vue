f369c030fefc94055bc0c4858fb8d436c56fd20f
Adán Estévez <adanescollante@gmail.com>
2023-05-25 16:43:16 -0400

    coded primary button component

---
+++ src/components/PrimaryButton.vue
@@ -0,0 +1,34 @@
+<template>
+  <button class="primary-btn">
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {
+  name: "PrimaryButton",
+};
+</script>
+
+<style>
+.primary-btn {
+  border: none;
+  background-color: var(--red);
+  color: var(--white);
+  font-size: calc(var(--font-size) * 1);
+  font-weight: var(--font-bold);
+  height: auto;
+  width: 100%;
+  margin: 0;
+  padding: 0.5rem 2rem;
+  border-radius: 2px;
+  box-shadow: 4px 4px 4px 0 rgba(0, 0, 0, 0.2);
+  cursor: pointer;
+  transition: all ease-out 0.2s;
+}
+
+.primary-btn:hover {
+  transform: translateY(-1px);
+  box-shadow: 4px 4px 4px 2px rgba(0, 0, 0, 0.2);
+}
+</style>
