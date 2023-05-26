1a31cbe6f44228e3dbbccd478ef9144ea4223648
Adán Estévez <adanescollante@gmail.com>
2023-05-26 01:17:00 -0400

    created a regular component layout

---
+++ src/layouts/ComponentLayout.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="component">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Component",
+};
+</script>
+
+<style>
+.component {
+  border-top: 1px solid var(--gray);
+  max-width: 800px;
+  margin: 0 auto;
+  padding: 2rem;
+}
+</style>
--- src/layouts/PrimaryLayout.vue
+++ src/layouts/PrimaryLayout.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="wrapper">
+  <div>
     <HeaderSection />
     <slot></slot>
     <FooterSection />
