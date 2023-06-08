318d27330041a08c29b0df3dabbb933e7c433722
Adán Estévez <adanescollante@gmail.com>
2023-06-08 11:12:58 -0400

    fixed padding error

--- src/layouts/ComponentLayout.vue
+++ src/layouts/ComponentLayout.vue
@@ -14,14 +14,14 @@ export default {
 .component {
   border-top: 1px solid var(--gray);
   max-width: 1000px;
-  margin: 2rem auto;
+  margin: 0 auto;
   padding: 2rem;
 }
 
 @media only screen and (min-width: 768px) {
   .component {
     border-top: none;
-    padding: 4rem 2rem;
+    padding: 4rem;
   }
 }
 </style>
