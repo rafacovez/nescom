196fbe7223a21f1aeb3b98bb63be2d1327aaf11c
Adán Estévez <adanescollante@gmail.com>
2023-06-11 22:24:24 -0400

    added illustration to desktop design of contact form

--- src/components/ContactSection.vue
+++ src/components/ContactSection.vue
@@ -1,85 +1,92 @@
 <template>
-  <ComponentLayout id="contactSection">
-    <h3>Envíanos un correo</h3>
-    <form action="">
-      <div class="form-group">
-        <template v-if="nameHasValue"></template>
-        <label
-          v-else
-          class="label-name"
-          for="name"
-          :class="{ focused: isFocusedName }"
-          >Nombre:</label
-        >
-        <input
-          class="input-name"
-          type="text"
-          id="name"
-          name="name"
-          autocomplete="off"
-          required
-          v-model="inputValueName"
-          @focus="isFocusedName = true"
-          @blur="isFocusedName = false"
-        />
-      </div>
-      <div class="form-group">
-        <template v-if="emailHasValue"></template>
-        <label
-          v-else
-          class="label-email"
-          for="email"
-          :class="{ focused: isFocusedEmail }"
-          >Correo:</label
-        >
-        <input
-          class="input-email"
-          type="email"
-          id="email"
-          name="email"
-          autocomplete="off"
-          required
-          v-model="inputValueEmail"
-          @focus="isFocusedEmail = true"
-          @blur="isFocusedEmail = false"
-        />
-      </div>
-      <div class="form-group">
-        <template v-if="messageHasValue"></template>
-        <label
-          v-else
-          class="label-message"
-          for="message"
-          :class="{ focused: isFocusedMessage }"
-          >Mensaje:</label
-        >
-        <textarea
-          class="input-message"
-          id="message"
-          name="message"
-          autocomplete="off"
-          required
-          v-model="inputValueMessage"
-          @focus="isFocusedMessage = true"
-          @blur="isFocusedMessage = false"
-        ></textarea>
-      </div>
-      <PrimaryButton class="submit-button">
-        <input type="submit" value="Enviar" />
-      </PrimaryButton>
-    </form>
+  <ComponentLayout class="contact-section">
+    <div class="contact-section__text">
+      <h3>Envíanos un correo</h3>
+      <form action="">
+        <div class="form-group">
+          <template v-if="nameHasValue"></template>
+          <label
+            v-else
+            class="label-name"
+            for="name"
+            :class="{ focused: isFocusedName }"
+            >Nombre:</label
+          >
+          <input
+            class="input-name"
+            type="text"
+            id="name"
+            name="name"
+            autocomplete="off"
+            required
+            v-model="inputValueName"
+            @focus="isFocusedName = true"
+            @blur="isFocusedName = false"
+          />
+        </div>
+        <div class="form-group">
+          <template v-if="emailHasValue"></template>
+          <label
+            v-else
+            class="label-email"
+            for="email"
+            :class="{ focused: isFocusedEmail }"
+            >Correo:</label
+          >
+          <input
+            class="input-email"
+            type="email"
+            id="email"
+            name="email"
+            autocomplete="off"
+            required
+            v-model="inputValueEmail"
+            @focus="isFocusedEmail = true"
+            @blur="isFocusedEmail = false"
+          />
+        </div>
+        <div class="form-group">
+          <template v-if="messageHasValue"></template>
+          <label
+            v-else
+            class="label-message"
+            for="message"
+            :class="{ focused: isFocusedMessage }"
+            >Mensaje:</label
+          >
+          <textarea
+            class="input-message"
+            id="message"
+            name="message"
+            autocomplete="off"
+            required
+            v-model="inputValueMessage"
+            @focus="isFocusedMessage = true"
+            @blur="isFocusedMessage = false"
+          ></textarea>
+        </div>
+        <PrimaryButton class="submit-button">
+          <input type="submit" value="Enviar" />
+        </PrimaryButton>
+      </form>
+    </div>
+    <div v-if="screenWidth >= 1080" class="contact-section__illustration">
+      <BusinessIllustration />
+    </div>
   </ComponentLayout>
 </template>
 
 <script>
 import ComponentLayout from "@/layouts/ComponentLayout.vue";
 import PrimaryButton from "./PrimaryButton.vue";
+import BusinessIllustration from "../assets/illustrations/BusinessIllustration.vue";
 
 export default {
   name: "ContactSection",
   components: {
     ComponentLayout,
     PrimaryButton,
+    BusinessIllustration,
   },
   data() {
     return {
@@ -89,7 +96,20 @@ export default {
       inputValueName: "",
       inputValueEmail: "",
       inputValueMessage: "",
+      screenWidth: window.innerWidth,
     };
+  },
+  mounted() {
+    this.setScreenWidth();
+    window.addEventListener("resize", this.setScreenWidth);
+  },
+  beforeUnmount() {
+    window.removeEventListener("resize", this.setScreenWidth);
+  },
+  methods: {
+    setScreenWidth() {
+      this.screenWidth = window.innerWidth;
+    },
   },
   computed: {
     nameHasValue() {
@@ -135,11 +155,11 @@ form {
 }
 
 .form-group > input,
-textarea {
+.form-group > textarea {
   outline: none;
   border: 1px solid var(--gray);
   background-color: var(--white);
-  border-radius: 2px;
+  border-radius: var(--border-radius-sm);
   touch-action: manipulation;
 }
 
@@ -164,5 +184,24 @@ textarea:focus {
   border: none;
   color: var(--white);
   pointer-events: none;
+}
+
+@media only screen and (min-width: 1080px) {
+  .contact-section {
+    display: flex;
+    flex-direction: row-reverse;
+    justify-content: space-between;
+    align-items: center;
+    gap: 8rem;
+  }
+
+  .contact-section__text {
+    flex-basis: 70%;
+  }
+
+  .contact-section__illustration {
+    flex-basis: 30%;
+    max-width: 250px;
+  }
 }
 </style>
