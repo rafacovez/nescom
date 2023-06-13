e30c1b63b60eb1fe373a8fc3318df437c45792fe
Adán Estévez <adanescollante@gmail.com>
2023-06-13 12:44:10 -0400

    moved fixed modals to layout template

--- src/layouts/PrimaryLayout.vue
+++ src/layouts/PrimaryLayout.vue
@@ -1,22 +1,26 @@
 <template>
-  <div>
-    <HeaderSection />
-    <main>
-      <slot></slot>
-    </main>
-    <FooterSection />
-  </div>
+  <SocialMediaModal />
+  <ScrollTrackingModal />
+  <HeaderSection />
+  <main>
+    <slot></slot>
+  </main>
+  <FooterSection />
 </template>
 
 <script>
 import HeaderSection from "../components/HeaderSection.vue";
 import FooterSection from "../components/FooterSection.vue";
+import SocialMediaModal from "../components/SocialMediaModal.vue";
+import ScrollTrackingModal from "../components/ScrollTrackingModal.vue";
 
 export default {
   name: "PrimaryLayout",
   components: {
     HeaderSection,
     FooterSection,
+    SocialMediaModal,
+    ScrollTrackingModal,
   },
 };
 </script>
