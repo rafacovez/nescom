0c32cafb6dbca3a30a9983a129f132a48a5f8f26
Adán Estévez <adanescollante@gmail.com>
2023-06-26 18:42:00 -0400

    modified navigateTo.js function to open links in a new tab

--- src/utils/navigateTo.js
+++ src/utils/navigateTo.js
@@ -1,16 +1,3 @@
-export function navigateToInstagram() {
-  window.location.href = "https://www.instagram.com/nescomrd";
-}
-
-export function navigateToFacebook() {
-  window.location.href = "https://www.facebook.com/nescomrd";
-}
-
-export function navigateToTwitter() {
-  window.location.href = "https://twitter.com/nescomrd";
-}
-
-export function navigateToYoutube() {
-  window.location.href =
-    "https://www.youtube.com/channel/UCK6yeR3uawJDFfUKMehdD1A";
+export function navigateTo(link) {
+  window.open(link, "_blank");
 }
