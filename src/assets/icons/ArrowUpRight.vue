3f2a044051eac9e0ef06723b2ca4f4d21f3b8210
Adán Estévez <adanescollante@gmail.com>
2023-06-26 12:47:56 -0400

    deleted clients and services folders and moved their content to ./assets

---
+++ src/assets/icons/ArrowUpRight.vue
@@ -0,0 +1,25 @@
+<template>
+  <ArrowUpRightIcon class="icon" />
+</template>
+
+<script>
+import { ArrowUpRightIcon } from "@heroicons/vue/20/solid";
+
+export default {
+  name: "ArrowUpRight",
+  components: {
+    ArrowUpRightIcon,
+  },
+};
+</script>
+
+<style>
+.icon {
+  width: 15px;
+  transition: all ease-out 0.2s;
+}
+
+a:hover > .icon {
+  transform: translate(5px, -5px);
+}
+</style>
