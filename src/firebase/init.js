a0d97408fca91f685cf9733239accb85429cc383
Adán Estévez <adanescollante@gmail.com>
2023-06-29 12:28:04 -0400

    made firebase folder config public and added local env variables to .gitignore

---
+++ src/firebase/init.js
@@ -0,0 +1,23 @@
+import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: process.env.VUE_APP_FB_API_KEY,
+  authDomain: process.env.VUE_APP_FB_AUTH_DOMAIN,
+  projectId: process.env.VUE_APP_FB_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_FB_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_FB_MESSAGING_SENDER_ID,
+  appId: process.env.VUE_APP_FB_APP_ID,
+  measurementId: process.env.VUE_APP_FB_MEASUREMENT_ID,
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
+
+// Initialize Analytics service
+// const analytics = getAnalytics(app);
+
+// Initialize Firestore service
+const db = getFirestore();
+export default db;
