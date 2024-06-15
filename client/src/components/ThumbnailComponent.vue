eb63280ff8e306a94d4c2e00f8e834e3ac842bd3
Adán Estévez <adanescollante@gmail.com>
2024-06-15 00:13:14 -0400

    created reusable thumbnail component

---
+++ client/src/components/ThumbnailComponent.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="thumbnail">
+    <input
+      v-show="isInput"
+      @change="onInputChange"
+      class="sr-only"
+      type="file"
+      id="imageInput"
+      accept="image/*"
+    />
+    <label class="thumbnail__label" v-show="isInput" for="imageInput">
+      {{ labelText }}
+    </label>
+    <img
+      class="thumbnail__img"
+      :class="{ post: !isInput }"
+      v-lazy="thumbnailSrc"
+      :alt="thumbnailAlt"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ThumbnailComponent",
+  props: {
+    thumbnailSrc: {
+      type: String,
+      default: "",
+    },
+    thumbnailAlt: {
+      type: String,
+      default: "Post thumbnail",
+    },
+    isInput: {
+      type: Boolean,
+      default: false,
+    },
+    labelText: {
+      type: String,
+      default: "Click aquí",
+    },
+    onInputChange: {
+      type: Function,
+      default: null,
+    },
+  },
+};
+</script>
+
+<style>
+.thumbnail {
+  position: relative;
+  border: 1px solid var(--foreground-color);
+  width: 100%;
+  padding-top: 66.67%;
+  border-radius: var(--border-radius-sm);
+  overflow: hidden;
+}
+
+.thumbnail__label {
+  position: absolute;
+  inset: 0;
+  display: grid;
+  place-items: center;
+  font-size: calc(var(--font-size) * 0.8);
+  color: var(--white);
+  background-color: var(--black);
+  cursor: pointer;
+  opacity: 0.6;
+}
+
+.thumbnail__img {
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 100%;
+  width: 100%;
+  object-fit: cover;
+  z-index: -1000;
+}
+</style>
