e895a38c4cee997fb31f5cb767f4af9973ee3cec
Adán Estévez <adanescollante@gmail.com>
2026-08-29 14:00:24 -0400

    hide contact navbar menu conditionally

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -8,8 +8,16 @@ register = template.Library()
 
 @register.simple_tag
 def get_site_navigation():
+    contact_page = (
+        StandardPage.objects.live()
+        .public()
+        .filter(slug__in=["contacto", "contact", "contact-us"])
+        .first()
+    )
+
     return {
         "blogs": BlogIndexPage.objects.live().public(),
         "categories": BlogCategory.objects.all().order_by("nombre"),
         "legal_pages": StandardPage.objects.live().public().order_by("title"),
+        "contact_page": contact_page,
     }
